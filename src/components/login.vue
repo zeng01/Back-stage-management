8569ba2fe5ee9bdc65019621aa055331ad1d8158
zeng01 <751903510@qq.com>
2019-05-08 11:31:28 +0800

    '用户登录验证'

--- src/components/login.vue
+++ src/components/login.vue
@@ -2,15 +2,15 @@
     <div class="login">
         <div class='center'>
             <h3>用户登录</h3>
-            <el-form label-position="top" label-width="80px">
-                <el-form-item label="账户">
-                    <el-input v-model="username"></el-input>
+            <el-form label-position="top" label-width="80px" :rules='loginRules' :model="loginForm" ref='loginForm'>
+                <el-form-item label="账户" prop='username'>
+                    <el-input v-model="loginForm.username"></el-input>
                 </el-form-item>
-                <el-form-item label="密码">
-                    <el-input v-model="password"></el-input>
+                <el-form-item label="密码" prop='password'>
+                    <el-input v-model="loginForm.password" type='password'></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="getLogin">登录</el-button>
+                    <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -22,19 +22,34 @@ export default {
     name:'login',
     data() {
         return {
-            username:'',
-            password:''
+            loginForm:{
+                username:'',
+                password:''
+            },
+           
+                loginRules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 5, max: 8, message: '长度在 5 到 8 个字符', trigger: 'change' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'change' }
+          ],
+            }
         }
     },
     methods: {
-        getLogin(){
-            if(this.username=='' || this.password==""){
-                this.$message.warning('用户名或者密码不能为空')
-                return
-            }
+        submitForm(formName){
+            // if(this.loginForm.username=='' || this.loginForm.password==""){
+            //     this.$message.warning('用户名或者密码不能为空')
+            //     return
+            // }
+            this.$refs[formName].validate((valid) => {
+          if (valid) {
             this.$axios.post('login',{
-                username:this.username,
-                password:this.password
+                username:this.loginForm.username,
+                password:this.loginForm.password
             }).then(res=>{
                 if(res.data.meta.status==200){
                     this.$message.success(res.data.meta.msg)
@@ -44,6 +59,12 @@ export default {
                     this.$message.error(res.data.meta.msg)
                 }
             })
+          } else {
+            
+            return false;
+          }
+        });
+            
         }
     },
     created() {
