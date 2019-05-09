ebf9f287a6b37155b60b18e880aa649643d606a6
zeng01 <751903510@qq.com>
2019-05-09 19:54:03 +0800

    'error页面'

---
+++ src/components/error.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='error'>
+        <img src="../assets/images/error.png" alt="">
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='scss'>
+body{
+    background: #f2f2f2;
+}
+.error{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    a{
+        font-size:14px;
+        color:#666;
+        text-decoration: none;
+    }
+}
+</style>
